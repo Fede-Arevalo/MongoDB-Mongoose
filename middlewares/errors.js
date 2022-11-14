cdfe233fb43ccddb81371a0f39b91acadc665e60
Fede-Arevalo <arevalo.feder@gmail.com>
2022-11-14 14:17:42 +0100

    Solución de error de tipado en errors.js

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -6,19 +6,19 @@ const handleValidationError = (err, res) => {
       chain += errors[i] + " || ";
     }
     const string = chain.slice(0, -4);
-    res.status(400).send({ message: string });
+    res.status(400).send({ messages: string });
   } else {
     res.status(400).send({ message: errors });
   }
 };
 
 const typeError = (err, req, res, next) => {
-  if (err.name === "ValidtionError")
+  if (err.name === "ValidationError")
     return (err = handleValidationError(err, res));
   else if (err.code === 11000) {
     res.status(400).send("El correo tiene que ser único");
   } else {
-    res.status(500).send("Hubo un problema");
+    res.status(500).send(`Hubo un problema`);
   }
 };
 
