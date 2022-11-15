06272f62ad41f672108b2f522a881d350b4b8004
Fede-Arevalo <arevalo.feder@gmail.com>
2022-11-15 10:01:47 +0100

    bug fix

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // const { MONGO_URI } = require("./keys");
-
+require("dotenv").config();
 
 
 const dbConnection = async () => {
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 // const { jwt_secret } = require("../config/keys");
+require("dotenv").config();
 
 const UserController = {
   async register(req, res, next) {
